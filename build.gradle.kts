ce99fd48cc6c67dc28d5e2114663784cbd610035
shionit <frontier.engine@gmail.com>
2021-04-08 01:32:04 +0900

    open Excel file by kotlin
    using Apache POI
    fix #3

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.poi:poi-ooxml:5.0.0")
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,17 @@
-fun main(args: Array<String>) {
-    println("What is your name?")
-    val name = readLine()
-    println("Hello $name")
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import java.io.FileInputStream
+import java.nio.file.Paths
+
+const val INPUT_FILE: String = "EntityDefinition.xlsx"
+
+fun main() {
+    FileInputStream(Paths.get(INPUT_FILE).toFile()).use {
+        val workbook = WorkbookFactory.create(it)
+        for (sheet in workbook.sheetIterator()) {
+            println(sheet.sheetName)
+            var name = sheet.getRow(0).getCell(1).stringCellValue
+            println("B1 $name")
+        }
+    }
+
 }
